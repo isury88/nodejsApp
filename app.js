ef325e491e58c4193714b5abdc0fa2be47252823
Riccardo Venturini <riccardoventurini88@hotmail.com>
2017-11-03 05:18:19 +0000

    Bower and bootstrap integration

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-import express from 'express';
+const express =  require('express');
 
-var hi = "Hello World";
-console.log(hi);
+const app = express();
+
+const port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+app.get('/books', function(req, res) {
+  res.send('Hello Book');
+});
+
+app.listen(port,  function(err) {
+   console.log('running server on port ' + port);
+ });
