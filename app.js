c6f5d0e30a9f4e20daee6e418bd983c9cb6df024
Riccardo Venturini <riccardoventurini88@hotmail.com>
2017-11-03 05:53:39 +0000

    latest commits

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express =  require('express');
+var express =  require('express');
 
-const app = express();
+var app = express();
 
-const port = 5000;
+var port = 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', function(req, res) {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 app.get('/books', function(req, res) {
-  res.send('Hello Book');
+    res.send('Hello Book');
 });
 
 app.listen(port,  function(err) {
-   console.log('running server on port ' + port);
- });
+    console.log('running server on port ' + port);
+});
