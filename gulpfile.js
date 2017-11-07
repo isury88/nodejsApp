63fe05dfe63e3c8461bcf983caabf415703e7f14
Riccardo Venturini <riccardoventurini88@hotmail.com>
2017-11-07 05:15:34 +0000

    gulpfile completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
@@ -15,6 +16,14 @@ gulp.task('style', function() {
 
 gulp.task('inject', function() {
     var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {read: false});
+
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
     var options = {
         bowerJson: require('./bower.json'),
         directory: './public.lib',
@@ -23,5 +32,22 @@ gulp.task('inject', function() {
 
     return gulp.src('./src/views/*.html')
           .pipe(wiredep(options))
+          .pipe(inject(injectSrc, injectOptions))
           .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style'], function() {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+    .on('restart', function(ev) {
+        console.log('Restarting...');
+    });
 });
