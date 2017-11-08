296e7dfeeceee14f3f564a2876f3ab77258c4ecb
Riccardo Venturini <riccardoventurini88@hotmail.com>
2017-11-08 06:14:26 +0000

    adding first route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express =  require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views','./src/views');
@@ -11,6 +12,18 @@ app.set('views','./src/views');
 // app.engine('.hbs', handlebars({extname: '.hbs'}));
 
 app.set('view engine', 'ejs');
+
+bookRouter.route('/')
+    .get(function(req, res) {
+        res.send('Hello Books');
+    });
+
+bookRouter.route('/single')
+        .get(function(req, res) {
+            res.send('Hello Single Book');
+        });
+
+app.use('/Books', bookRouter);
 
 app.get('/', function(req, res) {
     res.render('index', {title: 'Hello from render',
