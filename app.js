fb4d2173a9f3f714b6eb5ba3598ebbcac7d89412
Riccardo Venturini <riccardoventurini88@hotmail.com>
2017-11-09 06:06:01 +0000

    Routes integration

--- app.js
+++ app.js
@@ -13,9 +13,41 @@ app.set('views','./src/views');
 
 app.set('view engine', 'ejs');
 
+var books = [
+  {
+        title: 'Eloquent JavaScript, Second Edition',
+        author: 'Marijn Haverbeke',
+        description: 'JavaScript lies at the heart.'
+    },
+    {
+        title: 'Learning JavaScript Design Patterns',
+        author: 'Addy Osmani',
+        description: 'With Learning JavaScript Design Patterns.'
+    },
+    {
+        title: 'Speaking JavaScript',
+        author: 'Axel Rauschmayer',
+        description: 'Like it or not, JavaScript is everywhere these. '
+    },
+    {
+        title: 'Programming JavaScript Applications',
+        author: 'Eric Elliott',
+        description: 'Take advantage of JavaScript power to build robust.'
+    },
+    {
+        title: 'Understanding ECMAScript 6',
+        author: 'Nicholas C. Zakas',
+        description: 'ECMAScript 6 represents the biggest update to the core.'
+    }
+];
 bookRouter.route('/')
     .get(function(req, res) {
-        res.send('Hello Books');
+        res.render('books', {
+            title: 'Books',
+            nav: [{Link:'/Books', Text:'Books'},
+            {Link:'/Authors', Text:'Authors'}],
+            books: books
+        });
     });
 
 bookRouter.route('/single')
